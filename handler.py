82e53be5295ae8aaca38c3866dcfa83c07e2b7e2
MaoRodriguesJ <maorodriguesj@gmail.com>
2016-10-02 17:43:23 -0300

    Checking common between 2

--- handler.py
+++ handler.py
@@ -8,7 +8,7 @@ class Handler:
 	def possibilities(self, event1, event2):
 		for dates1 in event1.dates:
 			for dates2 in event2.dates:
-				if(!(dates1.conflict(dates2))):
+				if(not(dates1.conflict(dates2))):
 					self.possible_events.append('{} {} , {} {}'.format(
 						event1.name,
 						dates1,
--- hourtable.py
+++ hourtable.py
@@ -1,21 +1,47 @@
 import event
+import date
 import handler
 
 class HourTable:
 	def __init__(self):
 		self.events = []
+		self.common = []
+		self.possibilities = []
 
 #main
 exit = False
 hour_table = HourTable()
 while not exit:
-	case = input('\n1 Create event\n2 List events\n3 Check conflict\n4 Exit\n')
+	case = input("\n1 Create event\
+				  \n2 List events\
+				  \n3 Load Premade Events\
+				  \n4 Possible Combination\
+				  \n5 In Common COmbination\
+				  \n6 Exit\n")
 	if case == '1':
 		hour_table.events.append(event.Event.create_event())
+
 	if case == '2':
 		for k in hour_table.events:
 			print(k)
+
 	if case == '3':
+		name1 = local1 = author1 = 'a'
+		date1 = date.Date(2, '13:00', '14:00')
+		date2 = date.Date(3, '14:00', '15:00')
+		dates1 = [date1, date2]
+		event1 = event.Event(name1, dates1, local1, author1)
+
+		name2 = local2 = author2 = 'b'
+		date3 = date.Date(2, '14:00', '15:00')
+		date4 = date.Date(3, '14:00', '15:00')
+		dates2 = [date3, date4]
+		event2 = event.Event(name2, dates2, local2, author2)
+
+		hour_table.events.append(event1)
+		hour_table.events.append(event2)
+
+	if case == '4':
 		handler_test = handler.Handler()
 		case_event1 = int(input('Number of first event: '))
 		case_event2 = int(input('Number of second event: '))
@@ -24,5 +50,14 @@ while not exit:
 		for k in handler_test.possible_events:
 			print(k)
 
-	if case == '4':
+	if case == '5':
+		handler_test = handler.Handler()
+		case_event1 = int(input('Number of first event: '))
+		case_event2 = int(input('Number of second event: '))
+		handler_test.incommon(hour_table.events[case_event1],
+							  hour_table.events[case_event2])
+		for k in handler_test.possible_events:
+			print(k)
+
+	if case == '6':
 		exit = True
