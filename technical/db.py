4a746805dd8694412f4f07bc976178bc6d9f43ce
maorodriguesj <maorodriguesj@gmail.com>
2016-10-20 22:17:41 -0200

    trying db and handler on different package

---
+++ technical/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:///sqlalchemy.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+Session = DBSession()
